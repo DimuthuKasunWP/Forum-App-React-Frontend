2f5bfbc2986f855f4e73d231bb87990c618a8638
Dimuthu <kasunwpdimuthu@gmail.com>
2020-05-12 13:19:16 +0530

    combine reducer added

---
+++ src/store/modules/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from "redux"
+import authReducer  from './auth/reducer/authReducer'
+import { postsState }  from "./posts/reducer/postsReducer";
+import { likesState } from './likes/reducer/likesReducer'
+import { commentsState } from './comments/reducer/commentsReducer'
+
+
+const reducer = combineReducers({
+  Auth: authReducer,
+  PostsState: postsState,
+  LikesState: likesState,
+  CommentsState: commentsState
+})
+
+export default reducer
