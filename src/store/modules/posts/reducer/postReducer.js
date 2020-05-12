386c77f8c52db583068cba8d4e05ad73c1da3b15
Dimuthu <kasunwpdimuthu@gmail.com>
2020-05-12 13:05:24 +0530

    post reducer logic added

--- src/store/modules/posts/reducer/postReducer.js
+++ src/store/modules/posts/reducer/postReducer.js
@@ -1,154 +1,123 @@
-import { SIGNUP_SUCCESS, SIGNUP_ERROR, LOGIN_SUCCESS, LOGIN_ERROR, LOGOUT_SUCCESS, UPDATE_USER_AVATAR, UPDATE_USER_SUCCESS, UPDATE_USER_ERROR, BEFORE_STATE, UPDATE_USER_AVATAR_ERROR, BEFORE_AVATAR_STATE, BEFORE_USER_STATE, FORGOT_PASSWORD_SUCCESS, FORGOT_PASSWORD_ERROR, RESET_PASSWORD_SUCCESS, RESET_PASSWORD_ERROR, DELETE_USER_SUCCESS, DELETE_USER_ERROR  } from '../authTypes'
-import isEmpty from 'lodash/isEmpty';
+import { BEFORE_STATE_POST, FETCH_POSTS, FETCH_POSTS_ERROR, CREATE_POST_SUCCESS, UPDATE_POST_SUCCESS, CREATE_POST_ERROR, UPDATE_POST_ERROR, GET_POST_SUCCESS, GET_POST_ERROR, DELETE_POST_SUCCESS, DELETE_POST_ERROR, FETCH_AUTH_POSTS, FETCH_AUTH_POSTS_ERROR } from '../postsTypes'
 
 export const initState = {
-  isAuthenticated: false,
-  currentUser: {},
+  posts: [],
+  authPosts: [],
+  post: {},
+  postsError: null,
   isLoading: false,
-  isLoadingAvatar: false,
-  isUpdatingUser: false,
-  authError: null,
-  authSuccess: null
 }
 
-const authReducer = (state = initState, action) => {
-  switch(action.type) {
+export const postsState = (state = initState, action) => {
 
-    // This is the state to set when the button is click and we are waiting for response 
-    case BEFORE_STATE:
+  const { payload, type } = action
+  switch(type) {
+
+    case BEFORE_STATE_POST:
       return {
         ...state,
-        authError: null,
+        postsError: null,
         isLoading: true,
       }
-    case BEFORE_AVATAR_STATE:
-        return {
-          ...state,
-          avatarError: null,
-          isLoadingAvatar: true,
-        }
-    case BEFORE_USER_STATE:
-      return {
-        ...state,
-        userError: null,
-        isUpdatingUser: true,
-      }
-    case SIGNUP_SUCCESS:
-      return {
-        ...state,
+    case FETCH_POSTS:
+      return { 
+        ...state, 
+        posts: payload,
         isLoading: false,
-        signupError: null,
-        loginError: null
-
-      }
-    case SIGNUP_ERROR:
-      return {
-        ...state,
-        isLoading: false,
-        signupError: action.payload,
-        loginError: null
-
-      }
-    case LOGIN_SUCCESS:
-      return {
-        ...state,
-        isLoading: false,
-        currentUser: action.payload,
-        isAuthenticated: !isEmpty(action.payload),
-        loginError: null,
-        signupError: null,
-
-      }
-    case LOGIN_ERROR:
-      return {
-        ...state,
-        isLoading: false,
-        loginError: action.payload,
-        signupError: null,
-
-      }
-    case LOGOUT_SUCCESS:
-      return {
-        ...state,
-        isAuthenticated: false,
-        currentUser: {},
-        logoutError: null,
-        isLoading: false,
-        signupError: null,
-        loginError: null,
       }
       
-    case UPDATE_USER_AVATAR:
-      return {
-        ...state,
-        isLoadingAvatar: false,
-        currentUser: action.payload,
-        avatarError: null,
-        authSuccessImage: "Image Uploaded"
+    case FETCH_POSTS_ERROR:
+      return { 
+        ...state, 
+        postsError: payload,
+        isLoading: false 
       }
-    case UPDATE_USER_AVATAR_ERROR:
-        return {
-          ...state,
-          isLoadingAvatar: false,
-          avatarError: action.payload,
-      }
-    case UPDATE_USER_SUCCESS:
-      return {
-        ...state,
-        isUpdatingUser: false,
-        currentUser: action.payload,
-        userError: null,
-        authSuccessUser: "Details Updated"
-      }
-    case UPDATE_USER_ERROR:
-      return {
-        ...state,
-        isUpdatingUser: false,
-        userError: action.payload
-      }
-    case DELETE_USER_SUCCESS:
-      return {
-        ...state,
-        isAuthenticated: false,
-        currentUser: {},
+
+    case FETCH_AUTH_POSTS:
+      return { 
+        ...state, 
+        authPosts: payload,
         isLoading: false,
-        authSuccessUser: "User Deleted"
       }
-    case DELETE_USER_ERROR:
-      return {
-        ...state,
-        isLoading: false,
-        userError: action.payload
+
+    case FETCH_AUTH_POSTS_ERROR:
+      return { 
+        ...state, 
+        postsError: payload,
+        isLoading: false 
       }
-    case FORGOT_PASSWORD_SUCCESS:
-      return {
-        ...state,
-        isLoading: false,
-        forgotError: null,
-        successMessage: "Mesage sent to the email provided. Please check the spam folder"
+
+    case GET_POST_SUCCESS:
+      return { 
+        ...state, 
+        post: payload,
+        postsError: null,
+        isLoading: false  
       }
-    case FORGOT_PASSWORD_ERROR:
-      return {
-        ...state,
-        isLoading: false,
-        forgotError: action.payload
+
+    case GET_POST_ERROR:
+      return { 
+        ...state, 
+        postsError: payload,
+        isLoading: false 
       }
-      case RESET_PASSWORD_SUCCESS:
-      return {
-        ...state,
-        isLoading: false,
-        resetError: null,
-        successMessage: "Success! Password Reset" 
+
+    case CREATE_POST_SUCCESS:
+      return { 
+        ...state, 
+        posts: [payload, ...state.posts],
+        authPosts: [payload, ...state.authPosts],
+        postsError: null,
+        isLoading: false  
       }
-    case RESET_PASSWORD_ERROR:
-      return {
-        ...state,
-        isLoading: false,
-        resetError: action.payload
+
+    case CREATE_POST_ERROR:
+      return { 
+        ...state, 
+        postsError: payload,
+        isLoading: false  
       }
+
+    case UPDATE_POST_SUCCESS:
+      return { 
+        ...state, 
+        posts: state.posts.map(post => 
+          post.id === payload.id ? 
+          {...post, title: payload.title, content: payload.content } : post
+        ),
+        authPosts: state.authPosts.map(post => 
+          post.id === payload.id ? 
+          {...post, title: payload.title, content: payload.content } : post
+        ),
+        post: payload,
+        postsError: null,
+        isLoading: false 
+      }
+
+    case UPDATE_POST_ERROR:
+      return { 
+        ...state, 
+        postsError: payload,
+        isLoading: false  
+      }
+
+     case DELETE_POST_SUCCESS:
+      return { 
+        ...state, 
+        posts: state.posts.filter(post => post.id !== payload.deletedID),
+        authPosts: state.authPosts.filter(post => post.id !== payload.deletedID),
+        postsError: null,
+        isLoading: false   
+      }
+
+    case DELETE_POST_ERROR:
+      return { 
+        ...state, 
+        postsError: payload,
+        isLoading: false  
+      }
+
     default:
-      return state;
+      return state
   }
 }
-
-export default authReducer
