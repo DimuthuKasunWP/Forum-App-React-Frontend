ab6d997019a087d6ff81e788d5b6ec831ddecd2d
Dimuthu <kasunwpdimuthu@gmail.com>
2020-05-12 13:55:08 +0530

    navigation logic added

---
+++ src/components/Navigation.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
+
+import { SignOut } from '../store/modules/auth/actions/authAction';
+import Default from '../Assets/default.png'
+import './Navigation.css'
+
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
+
+
+const Navigation = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const currentState = useSelector((state) => state);
+  
+  const { isAuthenticated, currentUser } = currentState.Auth;
+
+  const dispatch = useDispatch()
+
+  const logoutUser  = () => dispatch(SignOut());
+
+
+
+  let imagePreview = null;
+  if(currentUser && currentUser.avatar_path){
+    imagePreview = (<img className="img_style_nav" src={currentUser.avatar_path} alt="profile 1"/>);
+  } else {
+    imagePreview = (<img className="img_style_nav" src={Default} alt="profile 2"/>);
+  }
+
+  const logout = (e) => {
+    e.preventDefault()
+    logoutUser()
+  }
+
+  const userProfile = isAuthenticated ?  `/profile/${currentState.Auth.currentUser.id}` : ""
+
+  const SignedInLinks = (
+              <React.Fragment>
+                  <NavItem className="mt-2" style={{marginRight: "15px" }}>
+                    <NavLink to="/createpost">Create Post</NavLink>
+                  </NavItem>
+                  <NavItem className="mt-2" style={{marginRight: "15px" }}>
+                    <NavLink to="/authposts">My Posts</NavLink>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>
+                    {imagePreview}
+                  </DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>
+                      <NavItem>
+                        <NavLink to={userProfile}>Profile</NavLink>
+                      </NavItem>
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      <a onClick={logout}>Logout</a>
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </React.Fragment>
+
+            )
+
+  const SignedOutLinks = (
+                <React.Fragment>
+                  <NavItem style={{marginRight: "20px" }}>
+                    <Link to='/login'>Login</Link>
+                  </NavItem>
+                  <NavItem>
+                    <Link to='/signup'>Signup</Link>
+                  </NavItem>
+                </React.Fragment>
+              )
+
+
+  return (
+    <div className="mb-3">
+      <Navbar color="light" light expand="md"> 
+          <NavbarBrand className="mx-auto" href="/">Seamflow</NavbarBrand>
+          <NavbarToggler onClick={() => setIsOpen(!isOpen) } /> 
+        <Collapse isOpen={isOpen} navbar> 
+          <Nav className="ml-auto" navbar>
+            { isAuthenticated ? SignedInLinks: SignedOutLinks }
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Navigation
